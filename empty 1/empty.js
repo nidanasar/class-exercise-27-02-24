a886b269e7e0f35d09cc1596e9f1b68dcbe24f44
nidanasar <159262333+nidanasar@users.noreply.github.com>
2024-02-29 01:26:23 +0500

    Add files via upload

---
+++ empty 1/empty.js
@@ -0,0 +1,7 @@
+var employeename = "Miss Nida";
+var Age = 32;
+var Accountant = true;
+var officeaddress = 12, x3, yz;
+var salary = null;
+var workingarea = undefined;
+console.log(employeename + Age + Accountant + officeaddress + salary + workingarea);
---
+++ empty 1/empty.ts
@@ -0,0 +1,7 @@
+let employeename: string = "Miss Nida";
+let Age:Number = 32
+let Accountant: boolean= true
+let officeaddress: any = 12 ,x3, yz
+let salary= null
+let workingarea = undefined
+console.log(employeename +  Age + Accountant + officeaddress + salary + workingarea);
